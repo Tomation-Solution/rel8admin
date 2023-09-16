a7eccd66cb5b686c7dfddceeec9ca263797bbb70
markothedev <markothedevmail@gmail.com>
2023-09-16 20:27:02 +0100

    added new backend url to axios

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -4,13 +4,10 @@ export const adminDetails = JSON.parse( localStorage.getItem("admin-user"))?.sta
 
 const TOKEN = adminDetails?.token
 
-// const SHORT_NAME = JSON.parse(localStorage.getItem("shortName"))
-const SHORT_NAME = 'nimn'
-export const URLnAME = 'localhost:8000'
-export const BASE_URL= `http://localhost:8000/tenant/${SHORT_NAME}`
+const SHORT_NAME = JSON.parse(localStorage.getItem("shortName"))
 
-// export const URLnAME = 'rel8backend-production.up.railway.app'
-// const BASE_URL = `https://rel8backend-production.up.railway.app/tenant/${SHORT_NAME}/`
+export const URLnAME = 'rel8backend-production-adfb.up.railway.app'
+const BASE_URL = `https://rel8backend-production-adfb.up.railway.app/tenant/${SHORT_NAME}/`
 
 export const privateRequest = axios.create({
     baseURL: BASE_URL,
