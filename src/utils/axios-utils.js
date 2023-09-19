b9a31fc39832ca5d8e561ae52eb4a4045f05982e
markothedev <markothedevmail@gmail.com>
2023-09-19 08:36:24 +0100

    .

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -3,11 +3,13 @@ import axios from "axios";
 export const adminDetails = JSON.parse( localStorage.getItem("admin-user"))?.state?.user
 
 const TOKEN = adminDetails?.token
+
 let SHORT_NAME = localStorage.getItem("shortName")
+
 if(SHORT_NAME){
     SHORT_NAME=JSON.parse(localStorage.getItem("shortName"))
 }
-console.log(SHORT_NAME)
+
 export const URLnAME = 'rel8backend-production-adfb.up.railway.app'
 export const BASE_URL = `https://rel8backend-production-adfb.up.railway.app/tenant/${SHORT_NAME}/`
 
